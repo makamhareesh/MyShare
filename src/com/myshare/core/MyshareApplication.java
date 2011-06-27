1cc74d31f02cdb3c0e95f3a0e2b91bd9fb35678f
unknown <makamhareesh@gmail.com>
2011-06-28 00:19:15 +0530

    new file added.

--- src/com/myshare/core/MyshareApplication.java
+++ src/com/myshare/core/MyshareApplication.java
@@ -1,35 +1,92 @@
 package com.myshare.core;
 
-import com.myshare.forms.MyLoginForm;
-import com.myshare.forms.RegisterForm;
 import com.vaadin.Application;
-import com.vaadin.terminal.ExternalResource;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
+import com.vaadin.terminal.Sizeable;
+import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.*;
-import com.vaadin.ui.UriFragmentUtility.FragmentChangedEvent;
-import com.vaadin.ui.UriFragmentUtility.FragmentChangedListener;
-import com.vaadin.ui.Window.CloseEvent;
-import com.vaadin.ui.Window.CloseListener;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 
-public class MyshareApplication extends Application {
+public class MyShareApplication extends Application implements
+ClickListener, ValueChangeListener, ItemClickListener {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -8348580599341774118L;
+	private NavigationTree tree = new NavigationTree(this);
+    private HorizontalSplitPanel horizontalSplit = new HorizontalSplitPanel();
+
+	
 	@Override
 	public void init() {
-		
-		setMainWindow(new MyShareWindow());
-		
+
+        buildMainLayout();
+
 /*		
 		MyLoginForm loginform = new MyLoginForm();
 		mainWindow.addComponent(loginform);
 		setMainWindow(mainWindow);
 */	
 		}
-	class MyShareWindow extends Window {
+	
+	private void buildMainLayout() {
+		// TODO Auto-generated method stub
 		
-		MyShareWindow(){
-			
-			RegisterForm userForm = new RegisterForm();
-			addComponent(userForm);
-			
-		}
+        setMainWindow(new Window("Whats My Share"));
+
+        setTheme("contacts");
+
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
+
+        layout.addComponent(createToolbar());
+        layout.addComponent(horizontalSplit);
+        layout.setExpandRatio(horizontalSplit, 1);
+
+        horizontalSplit.setSplitPosition(200, Sizeable.UNITS_PIXELS);
+        horizontalSplit.setFirstComponent(tree);
+
+        getMainWindow().setContent(layout);
+
+		
+	}
+	
+
+    private HorizontalLayout createToolbar() {
+        HorizontalLayout lo = new HorizontalLayout();
+
+        lo.setMargin(true);
+        lo.setSpacing(true);
+
+        lo.setStyleName("toolbar");
+
+        lo.setWidth("100%");
+
+        Embedded em = new Embedded("", new ThemeResource("images/logo.png"));
+        lo.addComponent(em);
+        lo.setComponentAlignment(em, Alignment.MIDDLE_RIGHT);
+        lo.setExpandRatio(em, 1);
+
+        return lo;
+    }
+
+	public void itemClick(ItemClickEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void valueChange(ValueChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void buttonClick(ClickEvent event) {
+		// TODO Auto-generated method stub
 		
 	}
 
